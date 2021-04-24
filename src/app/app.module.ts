191da237c6d315e04092dd279fafd99c338adab8
Mike Huang <wellwind@mail2000.com.tw>
2021-04-24 10:43:52 +0800

    任務 01：建立並設定路由機制

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/posts-routing.module.ts
@@ -0,0 +1,20 @@
+import { CreateComponent } from './create/create.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './post/post.component';
+
+import { PostsComponent } from './posts.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'posts' },
+  { path: 'posts', component: PostsComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'post', redirectTo: 'posts'},
+  { path: 'post/:id', component: PostComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PostsRoutingModule {}
